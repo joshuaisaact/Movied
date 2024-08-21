d77cd18e1f249cead7891cd747be1670eeab9e47
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-08-21 16:30:37 +0100

    finished basic look and feel, nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { MovieComponent } from './movie/movie.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, MovieComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    MovieComponent,
+    DashboardComponent,
+    NavBarComponent,
+  ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Movie } from '../movie';
 import { MovieComponent } from '../movie/movie.component';
 import { ApiClientService } from '../api-client.service';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   movies: Movie[] = [];
 
   constructor(private movieService: ApiClientService) {}
