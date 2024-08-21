00a5d542401e5caddf663ad722cdcb0786884c47
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-08-21 14:55:03 +0100

    defined movie interface, created mock movie array

---
+++ src/app/mock-movies.ts
@@ -0,0 +1,20 @@
+import { Movie } from './movie';
+
+export const MOVIES: Movie[] = [
+  {
+    original_title: 'Deadpool & Wolverine',
+    poster_path: '/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg',
+  },
+  {
+    original_title: 'Deadpool & Wolverine',
+    poster_path: '/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg',
+  },
+  {
+    original_title: 'Deadpool & Wolverine',
+    poster_path: '/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg',
+  },
+  {
+    original_title: 'Deadpool & Wolverine',
+    poster_path: '/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg',
+  },
+];
